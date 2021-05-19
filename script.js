d47a953baf8223f52cf1264042a5deb4a0ef33a1
Kunal Arora <77977499+Kunal9155@users.noreply.github.com>
2021-05-20 01:33:37 +0530

    g

--- script.js
+++ script.js
@@ -19,4 +19,59 @@ console.log("you entered "  +num);
 document.write("you gave value of num is  " +num);
 document.write("<br/>")
 document.writeln("you gave value of num is  " +num);
+var a=34;
+var b=45;
+var c= a+b;
+console.log("the addition is " +c);
+// document.write("the addition is " +c)
+var d= a-b;
+console.log("the subtraction is " +d);
+var e= a*b;
+console.log("the multiplication is " +e);
+var f= a/b;
+console.log("the divide is " +f);
+var g= a%b;
+console.log("the module is " +g);
 
+var dd = (a == b);
+console.log("equilaty between a and b is " +dd);
+
+var ff = (a<b);
+console.log("a is less than b  " +ff);
+
+var gg = (a>b);
+console.log("a is grater than b " +gg);
+
+var ff = (a<=b);
+console.log("a is less than or equal to  b  " +ff);
+
+var gg = (a>=b);
+console.log("a is grater than or equal to  b " +gg);
+
+var yy= (a!=b);
+console.log("a is not equal to " +yy)
+
+if (a ==4 && b==34)
+{
+  console.log("a is 4 and b is 34")
+}else{
+  console.log("a is not 4 and b is not 34")
+}
+
+if (!(a ==4 && b==34))
+{
+  console.log("a is 4 and b is 34")
+}else{
+  console.log("a is not 4 and b is not 34")
+}
+var age = prompt('enter you age ');
+console.log("you entereed your age is " +age);
+if(age >=18)
+{
+  alert('you can vote for india');
+}
+else if (age <18)
+{
+  alert('you cannot vote');
+}
+console.log('hello woels')
